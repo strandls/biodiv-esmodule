f63d564db2af3473c7675742dba2b90d187c613f
Harsh Zalavadiya <harsh@strandls.com>
2019-10-04 17:57:07 +0530

    :ambulance: added configure-pre-sdk command

--- @ci/build-and-deploy.sh
+++ @ci/build-and-deploy.sh
@@ -5,6 +5,7 @@ chmod +x @ci/maven-toolbox
 # Configure and build project
 ./@ci/maven-toolbox configure-properties src/main/resources/config.properties
 ./@ci/maven-toolbox configure-m2
+./@ci/maven-toolbox configure-pre-sdk
 mvn clean install
 ./@ci/maven-toolbox configure-sdk
 mvn -f target/sdk/pom.xml deploy
